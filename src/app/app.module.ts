c9360428426005430d5c93ecf878071d4b52edfa
Jora Gevorgyan <jora@ispras.ru>
2022-04-10 19:44:20 +0400

    the all components are already created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
+import { ProductsComponent } from './products/products.component';
+import { ChatComponent } from './chat/chat.component';
+import { BrokerComponent } from './broker/broker.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyComponent,
+    SellComponent,
+    ProductsComponent,
+    ChatComponent,
+    BrokerComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
