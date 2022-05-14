81e6a8b374d1124d909ebaa618074449fb18a72d
Jora Gevorgyan <jora@ispras.ru>
2022-05-15 03:18:32 +0400

    sell component template

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,13 +1,65 @@
 import {Component, OnInit} from '@angular/core';
+import {eventListeners} from "@popperjs/core";
 
 type Product = {
-    id: number;
     name: string;
     description: string;
     img: string;
     price: number;
     sellPercent: number;
 };
+
+let products: Array<Product> = [
+    {
+        name: 'jins',
+        description: 'barev sencel mna vat chi',
+        img: '/products/10.jpg',
+        price: 100,
+        sellPercent: 0,
+    },
+    {
+        name: 'heraxos',
+        description: 'vochinch patahuum a',
+        img: '/products/10.jpg',
+        price: 430,
+        sellPercent: 0,
+    },
+    {
+        name: 'backpack',
+        description: 'lavn a vonc vor',
+        img: '/products/10.jpg',
+        price: 200,
+        sellPercent: 0,
+    },
+    {
+        name: 'jins',
+        description: 'barev sencel mna vat chi',
+        img: '/products/10.jpg',
+        price: 100,
+        sellPercent: 0,
+    },
+    {
+        name: 'jins',
+        description: 'barev sencel mna vat chi',
+        img: '/products/10.jpg',
+        price: 100,
+        sellPercent: 0,
+    },
+    {
+        name: 'jins',
+        description: 'barev sencel mna vat chi',
+        img: '/products/10.jpg',
+        price: 100,
+        sellPercent: 0,
+    },
+    {
+        name: 'jins',
+        description: 'barev sencel mna vat chi',
+        img: '/products/10.jpg',
+        price: 100,
+        sellPercent: 20,
+    }
+];
 
 @Component({
     selector: 'app-buy',
@@ -18,90 +70,34 @@ type Product = {
 export class BuyComponent implements OnInit {
     public currentProducts: Array<Product> = [];
 
-    private products: Array<Product> = [
-        {
-            id: 1,
-            name: 'jins',
-            description: 'barev sencel mna vat chi',
-            img: '/products/10.jpg',
-            price: 100,
-            sellPercent: 0,
-        },
-        {
-            id: 2,
-            name: 'heraxos',
-            description: 'vochinch patahuum a',
-            img: '/products/10.jpg',
-            price: 430,
-            sellPercent: 0,
-        },
-        {
-            id: 3,
-            name: 'backpack',
-            description: 'lavn a vonc vor',
-            img: '/products/10.jpg',
-            price: 200,
-            sellPercent: 0,
-        },
-        {
-            id: 4,
-            name: 'jins',
-            description: 'barev sencel mna vat chi',
-            img: '/products/10.jpg',
-            price: 100,
-            sellPercent: 0,
-        },
-        {
-            id: 5,
-            name: 'jins',
-            description: 'barev sencel mna vat chi',
-            img: '/products/10.jpg',
-            price: 100,
-            sellPercent: 0,
-        },
-        {
-            id: 6,
-            name: 'jins',
-            description: 'barev sencel mna vat chi',
-            img: '/products/10.jpg',
-            price: 100,
-            sellPercent: 0,
-        },
-        {
-            id: 7,
-            name: 'jins',
-            description: 'barev sencel mna vat chi',
-            img: '/products/10.jpg',
-            price: 100,
-            sellPercent: 20,
-        },
-    ];
-
     constructor() {
-
-        this.currentProducts = this.products;
-
+        this.currentProducts = products;
+        console.log(products)
     }
 
     getCurrentPrice(product: Product): number {
         return product.price * (1 - product.sellPercent / 100);
     }
 
-    searchProducts(e: any) {
-        const val = e.value.toString().toLowerCase();
+    searchProducts(event: any) {
+        const val = event.target.value.toString().toLowerCase();
 
         if (!val) {
-            this.currentProducts = this.products;
+            this.currentProducts = products;
         } else {
-            this.currentProducts = this.products.filter(i => {
+            this.currentProducts = products.filter(i => {
                 return i.name.toLowerCase().indexOf(val) > -1 || i.description.toLowerCase().indexOf(val) > -1;
             })
         }
     }
 
+    setProduct(product: Product) {
+        products.push(product);
+    }
+
     getProducts(): Array<Product> {
         //todo get products from API
-        return this.products;
+        return products;
     }
 
     ngOnInit(): void {
--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BuyComponent} from "../buy/buy.component";
 
 @Component({
-  selector: 'app-sell',
-  templateUrl: './sell.component.html',
-  styleUrls: ['./sell.component.css']
+    selector: 'app-sell',
+    templateUrl: './sell.component.html',
+    styleUrls: ['./sell.component.css']
 })
 export class SellComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+    buyObj = new BuyComponent();
+    setPrd () {
+      let t =   {
+        name: 'jins',
+        description: 'barev sencel mna vat chi',
+        img: '/products/10.jpg',
+        price: 100,
+        sellPercent: 20,
+      };
+      this.buyObj.setProduct(t);
+      console.log(this.buyObj.getProducts());
+    }
+    ngOnInit(): void {
+    }
 
 }
