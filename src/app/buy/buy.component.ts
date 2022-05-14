6a29cb1e8b30282f580fc5f1fa01335c204037ab
Jora Gevorgyan <jora@ispras.ru>
2022-05-15 01:02:38 +0400

    show products from 'buy' component

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -7,7 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    console.log('sdfsdgfsdf')
+
+  }
+
+  private products = [
+    {
+      id: 1,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+    {
+      id: 2,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+    {
+      id: 3,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+    {
+      id: 4,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+    {
+      id: 5,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+    {
+      id: 6,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+    {
+      id: 7,
+      name: 'jins',
+      description: 'barev sencel mna vat chi',
+      img: '/products/10.jpg',
+      price: 100,
+      sellPercent: 0,
+    },
+  ];
+
+
+
+
+  getProducts():Array<any> {
+    //todo get products from API
+    return this.products;
+  }
 
   ngOnInit(): void {
   }
